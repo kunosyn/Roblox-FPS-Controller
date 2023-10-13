d308713b4f101ffc62540cdb455bfb7fef7e8dd7
kunosyn <65258314+kunosyn@users.noreply.github.com>
2023-10-12 22:53:15 -0500

    Delete StarterPlayer/ServerScriptService directory

--- StarterPlayer/ServerScriptService/FPSController/ClientTranslator.lua
+++
@@ -1,6 +0,0 @@
-ReplicatedStorage = game:GetService('ReplicatedStorage')
-Events = ReplicatedStorage:WaitForChild('Events')
-
-Events.FPSControllerTranslator.OnServerEvent:Connect(function ( player: Player, eventName: string, args: { any } )
-	
-end)
