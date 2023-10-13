245daafc25b3519964e39a7f154dca1dadea08eb
kunosyn <65258314+kunosyn@users.noreply.github.com>
2023-10-12 22:53:45 -0500

    Create ClientTranslator.lua

---
+++ ServerScriptService/FPSController/ClientTranslator.lua
@@ -0,0 +1,6 @@
+ReplicatedStorage = game:GetService('ReplicatedStorage')
+Events = ReplicatedStorage:WaitForChild('Events')
+
+Events.FPSControllerTranslator.OnServerEvent:Connect(function ( player: Player, eventName: string, args: { any } )
+	
+end)
